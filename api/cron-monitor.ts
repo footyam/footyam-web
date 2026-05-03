1e7219b0c73e3bf7d09274b08b20d38cfad91aed
uoogo5090 <horigo2020@gmail.com>
2026-05-03 09:59:10 +0900

    fix cron monitor to run highlight engine

--- api/cron-monitor.ts
+++ api/cron-monitor.ts
@@ -4,7 +4,6 @@ import { runHighlightMonitorOnce } from '../src/lib/highlight-engine.js';
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const result = await runHighlightMonitorOnce();
-
     return res.status(200).json(result);
   } catch (e) {
     return res.status(500).json({
